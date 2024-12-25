1ac68db593e60111dc69edd6047574601d64393d
Black Napalm <black@noreply.localhost>
2024-12-25 21:33:31 +0200

    fix(nanopc-t6): dts -> dtb

--- modules/boards/nanopc-t6.nix
+++ modules/boards/nanopc-t6.nix
@@ -9,5 +9,5 @@
   # add some missing deviceTree in friendlyarm/kernel-rockchip:
   # NanoPC-T6's deviceTree in friendlyarm/kernel-rockchip:
   # https://github.com/armbian/linux-rockchip/blob/rk-6.1-rkr4.1/arch/arm64/boot/dts/rockchip/rk3588-nanopc-t6.dts
-  hardware.deviceTree.name = "rockchip/rk3588-nanopc-t6.dts";
+  hardware.deviceTree.name = "rockchip/rk3588-nanopc-t6.dtb";
 }
